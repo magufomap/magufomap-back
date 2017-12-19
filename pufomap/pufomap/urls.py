062d66b5bdf90d6510a10c523f9625d05b530faf
Pablo Ruiz-Muzquiz <diacritica@gmail.com>
2017-12-19 19:51:22 +0100

    Added tag endpoint

--- pufomap/pufomap/urls.py
+++ pufomap/pufomap/urls.py
@@ -8,6 +8,7 @@ from api import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'pois', views.POIViewSet)
+router.register(r'tags', views.TagViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
