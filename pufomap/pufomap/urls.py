0f5db2e6224cf7bed94ef6bede8730a154ee1991
Yamila Moreno <yamila.ms@gmail.com>
2017-12-21 14:17:40 +0100

    etacsufob nimda

--- pufomap/pufomap/urls.py
+++ pufomap/pufomap/urls.py
@@ -21,6 +21,6 @@ router.register(r'changerequests', views.ChangeRequestViewSet)
 urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^editor/secret/place/', admin.site.urls),
+    url(r'^pufomapadmin/', admin.site.urls),
     url(r'^api/login', obtain_jwt_token),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
